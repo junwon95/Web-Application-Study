17b3fc795385f69fd0bf6626dfd8369fdeed3ea9
Junwon Seo <junwonseo95@gmail.com>
2020-08-03 13:25:43 +0900

    changed postgresql dependency position (<plugin> -> <dependencies>)

--- src/main/resources/db/mysql/user.sql
+++ src/main/resources/db/mysql/user.sql
@@ -4,4 +4,5 @@ ALTER DATABASE petclinic
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
 
+--****** might be a problem when deployed to Heroku (probably uses different username)
 GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic';
